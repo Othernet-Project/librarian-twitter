0bc64a9e78b03a3f2237c9f4806e575b2b2262ed
Branko Vukelic <branko@outernet.is>
2015-11-25 14:37:52 +0100

    Fixed migration query

--- librarian_twitter/migrations/twitter/00_01_init_twitter_database.py
+++ librarian_twitter/migrations/twitter/00_01_init_twitter_database.py
@@ -1,9 +1,9 @@
 SQL = """
 create table tweets
 (
-    id varchar primary_key unique not null,         -- ID
+    id varchar primary key not null,                -- ID
     handle varchar,                                 -- handle
-    text text,                                     -- tweets content
+    text text,                                      -- tweets content
     image varchar default '{}',                     -- image for the tweet
     created timestamp not null                      -- creation timestamp
 );
